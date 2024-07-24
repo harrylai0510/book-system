1fc1c70e4749f8cf87677c016a926110848562dc
Harry Lai <harrylai0510@gmail.com>
2024-07-24 19:13:28 +0800

    fixing when user 2nd submit will clear user id

--- frontend/src/app/(login)/history/page.js
+++ frontend/src/app/(login)/history/page.js
@@ -75,7 +75,7 @@ export default function History(props) {
 
     function handleCloseModel() {
         setFormData({
-            userId: null,
+            ...formData,
             bookId: null
         })
         setModelOpen(false);
