3b76254fb541061585a0b9981ca472ceb9a2a63c
Harry Lai <harrylai0510@gmail.com>
2024-07-24 19:11:54 +0800

    add migration seed file for init system use

--- backend/seeders/20240704110043-user.js
+++ backend/seeders/20240704110043-user.js
@@ -2,13 +2,17 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        const users = [{
-            lemail: 'admin@admin.com',
-            firstName: 'Admin',
-            lastName: 'Admin',
-            password: 'admin'
-        }];
-        return queryInterface.bulkInsert(users);
+        const users = [
+            {
+                lemail: 'admin@admin.com',
+                firstName: 'Admin',
+                lastName: 'Admin',
+                password: 'admin',
+                createdAt: new Date(),
+                updatedAt: new Date()
+            }
+        ];
+        return queryInterface.bulkInsert('Users', users, {});
     },
     async down(queryInterface, Sequelize) {
         return queryInterface.bulkDelete('Users', null, {});
--- backend/seeders/20240704110046-book.js
+++ backend/seeders/20240704110046-book.js
@@ -2,10 +2,12 @@
 
 module.exports = {
     async up(queryInterface, Sequelize) {
-        const books = [{
-            bookName: 'Book 00001'
-        }];
-        return queryInterface.bulkInsert(books);
+        const books = [
+            { bookName: '香港公屋設計變奏曲', createdAt: new Date(), updatedAt: new Date() },
+            { bookName: '香港商場的黃金時代', createdAt: new Date(), updatedAt: new Date() },
+            { bookName: '遊走香港屋邨誌',    createdAt: new Date(), updatedAt: new Date() },
+        ];
+        return queryInterface.bulkInsert('Books', books, {});
     },
     async down(queryInterface, Sequelize) {
         return queryInterface.bulkDelete('Books', null, {});
